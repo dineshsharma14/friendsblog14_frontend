b52e7f716c79184fcd9ee63d4abbfde298770144
dsharma <dinesh.sw3@gmail.com>
2021-02-03 09:46:57 -0600

    For deployment on Heroku

---
+++ src/app/server.js
@@ -0,0 +1,15 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/friends-blog-frontend'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', { root: 'dist/friends-blog-frontend/' }),
+);
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8082);
